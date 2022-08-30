9d8ba38646fefccd8a1b8935e04395a600c198a0
Megan <meganmoulos@gmail.com>
2022-08-30 09:16:09 -0700

    done with assignment

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ console.log(blogData);
 function App() {
   return (
     <div className="App">
-      <Header blogName={blogData.name}/>
+      <Header name={blogData.name}/>
       <About image={blogData.image} about={blogData.about}/>
       <ArticleList posts={blogData.posts}/>
     </div>
